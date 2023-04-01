6bef1643eed104c63c5efb30270068cd08bc57d7
Anzhelika <angelika.boichenko@outlook.com>
2023-04-01 17:39:27 +0300

    Убран отступ

--- cycle-while/while.js
+++ cycle-while/while.js
@@ -1,19 +1,13 @@
 let initialWeight = 6000; // Исходный вес в граммах
-let targetWeight = 5000; // Желанный вес в граммах
+const targetWeight = 5000; // Желанный вес в граммах
 let weight = 0;  // Сбрасываемый вес в граммах
-let realWeight = 0; // Реальный вес после тренировок в граммах
 let days = 0;
 
 while (initialWeight > targetWeight) {
   weight = initialWeight / 100 * 5;
-  realWeight = initialWeight - weight;
-  initialWeight = realWeight;
+  initialWeight = initialWeight - weight;
   days ++;
-  // console.log(realWeight);
-  
-  if (realWeight < targetWeight) {
-    break;  
-  } 
 }
-    console.log('Количество дней тренировок и диеты: ' + days);
+
+console.log('Количество дней тренировок и диеты: ' + days);
 // Программа окончена
